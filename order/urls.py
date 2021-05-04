6e8f03053799367a33a35ee9d3bae316510047c9
halil37 <hllibrahimsartitas@gmail.com>
2021-05-04 22:33:48 +0300

    user işlemleri yapıldı.

--- order/urls.py
+++ order/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.index, name='index'),
+   
     # path('shopcart/',views.shopcart, name="shopcart")
     # path('addtocart/<int:id>',views.addtocart,name="addtocart"),
     # path('deletetocart/<int:id>',views.deletetocart,name="deletetocart")
--- user/urls.py
+++ user/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user-profile/',views.index, name="profile")
-    # path('login/', views.login, name= 'login'),
-    # path('register/', views.register, name= 'register'),
-    # path('logout/', views.logout, name= 'logout'),
+    path('user-profile/',views.index, name="profile"),
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.user_password, name='user_password'),
+    path('orders/', views.user_orders, name='user_orders'),
+    path('orderdetail/<int:id>', views.user_orderdetail, name='user_orderdetail'),
+    path('comments/', views.user_comments, name='user_comment'),
+    path('deletecomment/<int:id>', views.user_deletecomment, name='delete_comment'),
 ]
